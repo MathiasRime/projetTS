4f51088da22c7a2c05f45c8de02d67a46113b1f9
MathiasRime <mathreine@gmail.com>
2021-04-06 16:38:31 +0200

    main

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var Pokemon_1 = require("./Classe/Pokemon");
+var p = new Pokemon_1.Pokemon("Carapute");
+console.log(p.name);
---
+++ main.ts
@@ -0,0 +1,4 @@
+import {Pokemon} from "./Classe/Pokemon";
+
+const p = new Pokemon("Carapute");
+console.log(p.name);
